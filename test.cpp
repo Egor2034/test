c4863d906fec60662d4a1dc14a9fa64e2b58f28d
Egor2034 <egor_2061@mail.ru>
2025-09-13 23:49:49 +0400

    faf

--- test.cpp
+++ test.cpp
@@ -1,8 +1,40 @@
 #include <iostream>
 
+class Animal {
+public:
+    Animal() : weight(-1), name("") {}
+
+    Animal(std::string name, float weight) {
+        this->name = name;
+        this->weight = weight;
+    }
+
+    void print() {
+        std::cout << name << ": " << weight << "\n";
+    }
+private:
+    float weight;
+    std::string name;
+};
+
+class Cat : public Animal {
+public:
+    Cat() : Animal() {}
+    Cat(std::string name, float weight) : Animal(name, weight) {}
+};
+
+class Dog : public Animal {
+public:
+    Dog() : Animal() {}
+    Dog(std::string name, float weight) : Animal(name, weight) {}
+};
+
 int main() {
-    int a = 4;
-    std::cout << a + 52 << "\n";
-    std::cout << "Hello, world!\n";
+    Cat cat{"Стёпа", 5.5};
+    Dog dog("Боня", 45);
+
+    cat.print();
+    dog.print();
+
     return 0;
 }
